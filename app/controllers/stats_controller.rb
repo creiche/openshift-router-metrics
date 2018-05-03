aa2583f53ff178edf663b839d1ded31b33fd9141
Chris Reiche <christopher.reiche@pb.com>
2018-05-03 12:45:33 -0400

    checked column in row

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -69,7 +69,7 @@ class StatsController < ApplicationController
     be_regex = /^be_.*_([^_]*)_([^_]*)$/
 
     response = CSV.parse router_conn.get('/;csv').body, headers: true
-    response.delete_if { |row| be_regex.match(row['# pxname']).nil? }
+    response.delete_if { |row| be_regex.match(col['# pxname']).nil? }
     logger.debug response
     response['project'] = response['# pxname'].collect{ |pxname| be_regex.match(pxname).captures[0] }
     response['route'] = response['# pxname'].collect{ |pxname| be_regex.match(pxname).captures[1] }
