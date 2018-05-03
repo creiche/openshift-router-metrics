0bb383f6a19047f8cb279db26c71df9fff5081ac
Chris Reiche <christopher.reiche@pb.com>
2018-05-03 14:05:08 -0400

    New regexp

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -66,12 +66,12 @@ class StatsController < ApplicationController
     end
     router_conn.basic_auth username, password
 
-    be_regex = /^be_.*$/
+    be_regex = /^(be_.*?):(.*?):(.*?)/
 
     response = CSV.parse router_conn.get('/;csv').body, headers: true
     response.delete_if { |row| be_regex.match(row['# pxname']).nil? }
-    response['project'] = response['# pxname'].collect{ |pxname| be_regex.match(pxname).captures[0] }
-    response['route'] = response['# pxname'].collect{ |pxname| be_regex.match(pxname).captures[1] }
+    response['project'] = response['# pxname'].collect{ |pxname| be_regex.match(pxname).captures[1] }
+    response['route'] = response['# pxname'].collect{ |pxname| be_regex.match(pxname).captures[2] }
 #    response.delete('# pxname')
     response
   end
